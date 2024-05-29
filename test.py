7dd3a93ed0df0b99ac2468679a648a8ad74dd825
ANDRE892 <ANDRE363636@mail.ru>
2024-05-29 11:30:41 +0300

    fix

---
+++ test.py
@@ -0,0 +1,63 @@
+import requests
+import base64
+import time
+from random import randint
+from aiogram import executor, Dispatcher, Bot, types
+
+API_KEY = "7190129804:AAHimHvNIR3Qa2oGo-O_ZukHgjZOyssysBM"
+bot = Bot(token=API_KEY)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands = 'start')
+async def func_start(message: types.Message):
+    await message.answer("Приветик. Я твой шедеврум за 20 копеек.")
+
+def generate_image(prompt_text):
+    prompt = {
+            "modelUri": "art://b1g3f13cj7d6d3ss2md9/yandex-art/latest",
+            "generationOptions": {
+                "seed": randint(10000, 20000000000)
+        },
+        "messages": [
+          {
+            "weight": 1,
+            "text": prompt_text
+          }
+        ]
+    }
+
+    url =  "https://llm.api.cloud.yandex.net/foundationModels/v1/imageGenerationAsyn"
+    headers = { #greger
+        "Content-Type": "application/json",
+        "Authorization": "Api-Key AQVNyT4NfggL6Tr_wJ4law22tPgQwvBEbwRe4IAm"
+    }
+
+    response = requests.post(url=url, headers=headers, json=prompt)
+    result = response.json()
+    operation_id = result['id']
+
+    operation_url = f"https://llm.api.cloud.yandex.net:443/operations/{operation_id}"
+
+    while True:
+        operation_response = requests.get(operation_url, headers=headers)
+        operation_result = operation_response.json()
+        if 'response' in operation_result:
+            image_base64 = operation_result['response']['image']
+            image_data = base64.b64decode(image_base64)
+            return image_data
+        else:
+            time.sleep(5)
+
+@dp.message_handler()
+async def handle_message(message: types.Message):
+    user_text = message.text
+    await message.reply('Генерирую, жди.')
+    #try:
+    image_data = generate_image(user_text)
+    await message.reply_photo(photo=image_data)
+    #except Exception as e:
+    #await message.reply(f"Произошла ошибка, wallahi... {e}")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
